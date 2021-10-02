fb96f3386a615a61759fecbbac201db7c18a13d6
clfo2014@gmail.com <clfo2014@gmail.com>
2021-10-02 10:13:35 -0400

    Update: Shiny App

--- script-song-recommender-I.R
+++ script-song-recommender-I.R
@@ -7,40 +7,43 @@ library(tidyverse)
 
 # Establish Connection With Spotify API ----
 get_spotify_authorization_code(
-    client_id = Sys.getenv("SPOTIFY_CLIENT_ID"),
-    client_secret = Sys.getenv("SPOTIFY_CLIENT_SECRET")
+    client_id = Sys.getenv("SPOTIFY_CLIENT_ID"), # <- Your Spotify Client ID
+    client_secret = Sys.getenv("SPOTIFY_CLIENT_SECRET") # <- Your Spotify Secret
 )
 
 # Get Playlist URL ----
-playlist_username <- playlist_username
-playlist_url      <- url
+playlist_username <- playlist_username # <- Your Spotify username
+playlist_url      <- url # <- The playlist url
+
 
 # Get Playlist Details ----
-playlist_songs_raw <- get_playlist_audio_features(
+playlist_raw <- get_playlist_audio_features(
     playlist_username,
     url
 ) 
 
-playlist_songs_raw %>% glimpse()
+
 
 # Create Numeric Values for Key Name, Mode Name and Key Mode
-key_name_tbl <- playlist_songs_raw %>% 
+key_name_tbl <- playlist_raw %>% 
     distinct(key_name) %>% 
     rowid_to_column() %>% 
     rename(key_name_id = rowid)
 
-mode_name_tbl <- playlist_songs_raw %>% 
+mode_name_tbl <- playlist_raw %>% 
     distinct(mode_name) %>% 
     rowid_to_column() %>% 
     rename(mode_name_id = rowid)
 
-key_mode_tbl <- playlist_songs_raw %>% 
+key_mode_tbl <- playlist_raw %>% 
     distinct(key_mode) %>% 
     rowid_to_column() %>% 
     rename(key_mode_id = rowid)
 
+
+
 # Final Dataframe
-playlist_songs_clean <- playlist_songs_raw %>% 
+playlist_clean_tbl <- playlist_raw %>% 
     
     # Select Needed Columns
     select(track.name, track.artists, danceability:tempo, key_name:key_mode) %>% 
@@ -61,20 +64,23 @@ playlist_songs_clean <- playlist_songs_raw %>%
     rename(track_id = rowid) %>% 
     setNames(names(.) %>% str_replace_all("\\.", "_"))
 
-playlist_songs_clean %>% glimpse()
+
 
 # Select "Reference" Song ----
-ref_song <- playlist_songs_clean %>% 
+ref_song <- playlist_clean_tbl %>% 
     filter(artist_name == "Niqo Nuevo" & track_name == "Ocean") 
 
 ref_song_id <- ref_song %>% pull(track_id)
 
+
 # Move "Reference" Song to Bottom of Dataset ----
-prepared_tbl <- playlist_songs_clean %>% 
+prepared_tbl <- playlist_clean_tbl %>% 
     filter(track_id != ref_song_id) %>% 
     rbind(ref_song) 
 
 prepared_tbl %>% tail()
+
+# prepared_tbl %>% write_rds(file = "prepared_tbl.rds")
 
 # Get Track Name & Track Title ----
 prepared_tbl_track_name <- prepared_tbl %>% 
@@ -86,26 +92,37 @@ prepared_tbl_numeric <- prepared_tbl %>%
     select(-track_id)
 
 # Scale Numeric Features ----
-set.seed(100)
 prepared_tbl_scaled <- prepared_tbl_numeric %>% 
     mutate_all(., ~ (scale(., center = TRUE) %>% as.vector))
     
-
 # Calculate Euclidean Distance From Reference Song
-set.seed(200)
-prepared_tbl_scaled$distance <- 
-    as.matrix(dist(prepared_tbl_scaled))[nrow(prepared_tbl_scaled),]
+distance_tbl <- as.matrix(dist(prepared_tbl_scaled))[nrow(prepared_tbl_scaled),] %>% 
+    as_tibble()
 
-prepared_tbl_scaled %>% tail()
+distance_tbl %>% tail()
 
+# distance_tbl %>% write_rds(file = "distance.rds")
+
+# Combine Distance, Artist Name/Track Name & Scaled Features ----
 prepared_tbl_dist <- prepared_tbl_track_name %>% 
     bind_cols(prepared_tbl_scaled) %>% 
-    filter(track_id != ref_song_id)
+    bind_cols(distance_tbl) %>% 
+    filter(track_id != ref_song_id) %>% 
+    rename(distance = value)
 
 prepared_tbl_dist %>% tail()
 
+# prepared_tbl_dist %>% write_rds(file = "prepared_tbl_dist.rds")
 
-prepared_tbl_dist %>% 
-    arrange(distance) %>% 
-    slice(1:10)
-    
+
+# Combine Distance, Artist Name/Track Name & Scaled Features ----
+results_tbl <- prepared_tbl_track_name %>% 
+    bind_cols(prepared_tbl_numeric) %>%
+    bind_cols(distance_tbl) %>% 
+    arrange(value)
+
+
+# Analyze Results - Top 5 Closest Songs ----
+results_tbl %>% arrange(value) %>% 
+    filter(track_id != ref_song_id) %>% 
+    head(5)
